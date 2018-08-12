82d5130db915c829b80b52770c45b49fec3f1f53
Christian <cvernikoff@gmail.com>
2018-08-12 16:30:54 -0700

    progressive webapp

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {Provider} from 'mobx-react'
 import CourseStore from './stores/CourseStore'
 import AuthStore from './stores/AuthStore'
 import {BrowserRouter} from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
     <BrowserRouter>
@@ -13,3 +15,4 @@ ReactDOM.render(
         </Provider>
     </BrowserRouter>
     , document.getElementById('root'));
+registerServiceWorker();
