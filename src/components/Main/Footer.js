f108c3bd30398e259a79c5f36326534d9b06e62f
Christian <cvernikoff@gmail.com>
2018-08-17 20:10:17 -0700

    working on iphone se

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
             <Segment inverted vertical style={{borderRadius: 10}}>
                 <Container textAlign='center'>
                     <h4 className="ui inverted header">&copy; Copyright 2018 | BobcatCourses</h4>
-                    Frontend: <a target="_blank" rel="noopener noreferrer"
-                                 href="https://github.com/dragonbone81/react-bobcat-courses"><Icon
+                    Front: <a target="_blank" rel="noopener noreferrer"
+                              href="https://github.com/dragonbone81/react-bobcat-courses"><Icon
                     size="big" name="github square" link/></a>
-                    Backend: <a target="_blank" rel="noopener noreferrer"
-                                href="https://github.com/dragonbone81/bobcat-courses-backend"><Icon
+                    Back: <a target="_blank" rel="noopener noreferrer"
+                             href="https://github.com/dragonbone81/bobcat-courses-backend"><Icon
                     size="big" name="github square" link/></a>
                     About: <Link to="/about"><Icon size="big" name="paw"/></Link>
                 </Container>
