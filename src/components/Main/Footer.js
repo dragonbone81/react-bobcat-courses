be4441690459768537eefc1a9cb80393ba79e48f
Christian <cvernikoff@gmail.com>
2018-08-17 20:00:13 -0700

    i think the background and footer are finaly done

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -4,18 +4,20 @@ import {Link} from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <Segment inverted vertical style={{minHeight: '8vh'}}>
-            <Container textAlign='center'>
-                <h4 className="ui inverted header">&copy; Copyright 2018 | BobcatCourses</h4>
-                Frontend: <a target="_blank" rel="noopener noreferrer"
-                             href="https://github.com/dragonbone81/react-bobcat-courses"><Icon
-                size="big" name="github square" link/></a>
-                Backend: <a target="_blank" rel="noopener noreferrer"
-                            href="https://github.com/dragonbone81/bobcat-courses-backend"><Icon
-                size="big" name="github square" link/></a>
-                About: <Link to="/about"><Icon size="big" name="paw"/></Link>
-            </Container>
-        </Segment>
+        <div className="footer">
+            <Segment inverted vertical style={{minHeight: '8vh'}}>
+                <Container textAlign='center'>
+                    <h4 className="ui inverted header">&copy; Copyright 2018 | BobcatCourses</h4>
+                    Frontend: <a target="_blank" rel="noopener noreferrer"
+                                 href="https://github.com/dragonbone81/react-bobcat-courses"><Icon
+                    size="big" name="github square" link/></a>
+                    Backend: <a target="_blank" rel="noopener noreferrer"
+                                href="https://github.com/dragonbone81/bobcat-courses-backend"><Icon
+                    size="big" name="github square" link/></a>
+                    About: <Link to="/about"><Icon size="big" name="paw"/></Link>
+                </Container>
+            </Segment>
+        </div>
     )
 };
 
